cf9d92b5f5df6850d70d4048f7ffbe102dbd8dde
PaneladaDe87 <102541192+PaneladaDe87@users.noreply.github.com>
2023-04-29 23:54:46 -0300

    Update main.py

--- source/main.py
+++ source/main.py
@@ -8,6 +8,10 @@ HEIGHT = 600
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 CAPTION = pygame.display.set_caption("T-Engine")
 
+class LOAD_ICON:
+    def __init__():
+        ICON = pygame.image.load_image("./icon.png")
+
 class RUN:
     def START(self):
         self.RUNNING = True
@@ -16,6 +20,8 @@ class RUN:
                 if EVENT.type == pygame.QUIT:
                     self.RUNNING = False
                     pygame.quit()
+                    
+            LOAD_ICON()
                     
 if __name__ == '__main__':
     GAME = RUN()
