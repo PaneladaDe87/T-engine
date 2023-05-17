0e5ae5bef35ade67b283b16647ba9aec3ee85dc0
PaneladaDe87 <102541192+PaneladaDe87@users.noreply.github.com>
2023-05-16 23:53:04 -0300

    Create build.py

---
+++ build.py
@@ -0,0 +1,16 @@
+import os
+import shutil
+
+def main():
+    os.system("python -m compileall -b src")
+  
+    apk_dir = "./debug"
+    os.makedirs(apk_dir, exist_ok=True)
+  
+    apk_path = "./debug/engine.apk"
+    shutil.copy(apk_path, apk_dir)
+  
+    print("tudo certo :D")
+  
+if __name__ == "__main__":
+    main()
