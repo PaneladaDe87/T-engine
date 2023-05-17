a20ee4d5a96c461e532d2f201366c463cfaa78fd
PaneladaDe87 <102541192+PaneladaDe87@users.noreply.github.com>
2023-05-17 17:14:14 -0300

    Update build.py

--- build.py
+++ build.py
@@ -3,14 +3,11 @@ import shutil
 
 def main():
     os.system("python -m compileall -b src")
-  
-    apk_dir = "debug/bin"
-    os.makedirs(apk_dir, exist_ok=True)
-  
-    apk_path = "debug/bin/debug.apk"
-    shutil.copy(apk_path, apk_dir)
-  
+    
+    apk_path = "debug.apk"
+    shutil.copy(apk_path)
+    
     print("tudo certo :D")
-  
+    
 if __name__ == "__main__":
     main()
