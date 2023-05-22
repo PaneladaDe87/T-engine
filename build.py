cd81d404f702d8bacd1088b8190b6f80e01fbdae
PaneladaDe87 <102541192+PaneladaDe87@users.noreply.github.com>
2023-05-22 13:16:44 -0300

    Update build.py

--- build.py
+++ build.py
@@ -3,10 +3,10 @@ import shutil
 
 def main():
     os.system("python -m compileall -b source")
-    apk_dir = "./bin/debug"
+    apk_dir = "debug/apk"
     os.makedirs(apk_dir, exist_ok=True)
     
-    apk_path = "./bin/debug/app-debug.apk"
+    apk_path = "debug/apk/app-debug.apk"
     shutil.copy(apk_path, apk_dir)
     
     print("tudo certo :D")
